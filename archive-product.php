e107061b7e5c5544cb586008f045435529599d9c
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-07-31 14:54:51 +0200

    wip

--- archive-product.php
+++ archive-product.php
@@ -55,7 +55,8 @@ get_header( 'shop' ); ?>
     <section class="mb-[90px]">
         <hr class="w-[194px] border-[#DDDDDD] mx-auto">
          <div class="h-[87px] lg:h-[112px] w-fit mx-auto mt-[50px] md:mt-[60px] lg:mt-[40px] xl:mt-[50px]">
-            <img src="https://staging.salgabriel.com/wp-content/uploads/2024/07/SalGabriel-Smybol.png" alt="" class="w-[113px] h-[113px]">
+            <img src="https://staging.salgabriel.com/wp-content/uploads/2024/07/SalGabriel-Smybol.png" alt="" class="h-[87px] lg:h-[112px] w-[87px] lg:w-[112px]">
+            
         </div>
 
     </section>
--- single-product.php
+++ single-product.php
@@ -513,8 +513,8 @@ if ( post_password_required() ) {
 
     <section class="mb-[90px] mt-[180px]">
         <hr class="w-[194px] border-[#DDDDDD] mx-auto">
-         <div class="h-[87px] lg:h-[112px] w-fit mx-auto mt-[50px] md:mt-[60px] lg:mt-[40px] xl:mt-[50px]">
-            <img src="https://staging.salgabriel.com/wp-content/uploads/2024/07/SalGabriel-Smybol.png" alt="" class="h-full">
+        <div class="h-[87px] lg:h-[112px] w-fit mx-auto mt-[50px] md:mt-[60px] lg:mt-[40px] xl:mt-[50px]">
+            <img src="https://staging.salgabriel.com/wp-content/uploads/2024/07/SalGabriel-Smybol.png" alt="" class="h-[87px] lg:h-[112px] w-[87px] lg:w-[112px]"> 
         </div>
 
     </section>
